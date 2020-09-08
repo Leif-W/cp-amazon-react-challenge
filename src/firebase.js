bcaf6ef8766c50c9a2a545340409dc97a4a6954e
Leif-W <Leif-W@users.noreply.github.com>
2020-09-08 15:55:43 -0400

    Setup Firebase configuration data

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyArOi_vp_K9DmaCs0K6jA9r03NE44J9lSE",
+    authDomain: "cp-amzn-react-challenge.firebaseapp.com",
+    databaseURL: "https://cp-amzn-react-challenge.firebaseio.com",
+    projectId: "cp-amzn-react-challenge",
+    storageBucket: "cp-amzn-react-challenge.appspot.com",
+    messagingSenderId: "213650439552",
+    appId: "1:213650439552:web:075c40f155894b9f969565"
+  };
+  
