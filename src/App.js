6bca8edaa2ff3153c81d2286c76ac0844961311a
Leif-W <Leif-W@users.noreply.github.com>
2020-09-08 16:20:14 -0400

    Use BEM CSS naming convention

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    // CSS BEM: App -> app
+    <div className="app">
       <h1><span role="img" aria-label="emojis fire rocket">🚀🔥</span>Hello Clever Programmers, let's build the Amazon store!<span role="img" aria-label="emojis fire rocket">🔥🚀</span></h1>
     </div>
   );
