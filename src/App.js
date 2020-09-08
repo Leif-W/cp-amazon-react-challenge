97882d356a5bd5f0cdd4b8498dfccad45d85c2b5
Leif-W <Leif-W@users.noreply.github.com>
2020-09-08 17:46:10 -0400

    Add initial Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // CSS BEM: App -> app
-    <div className="app">
-      <h1><span role="img" aria-label="emojis fire rocket">🚀🔥</span>Hello Clever Programmers, let's build the Amazon store!<span role="img" aria-label="emojis fire rocket">🔥🚀</span></h1>
+    <div className='app'>
+      <h1>
+        <span role='img' aria-label='emojis fire rocket'>
+          🚀🔥
+        </span>
+        Hello Clever Programmers, let's build the Amazon store!
+        <span role='img' aria-label='emojis fire rocket'>
+          🔥🚀
+        </span>
+      </h1>
+      <Header/>
+      {/* Home */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <img src='./amazon_header_logo_1024x373.png' className='header__logo'/>
+            <div className='header__search'>
+                <input type='text' className='header__search_input'/>
+                {/* Search Icon */}
+            </div>
+            <div className='header__nav'>
+                <div className='header__option'>
+                    <span className='header__option_line1'>Hello Guest</span>
+                    <span className='header__option_line2'>Sign In</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__option_line1'>Return</span>
+                    <span className='header__option_line2'>&amp; Orders</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__option_line1'>Your</span>
+                    <span className='header__option_line2'>Prime</span>
+                </div>
+                <div className='header__option'>
+                    {/* Basket Icon */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
