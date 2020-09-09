d08ffe0960e00c00485d55931744190586700f9f
Leif-W <Leif-W@users.noreply.github.com>
2020-09-09 13:17:21 -0400

    Add header with logo and css

--- src/App.js
+++ src/App.js
@@ -6,16 +6,7 @@ function App() {
   return (
     // CSS BEM: App -> app
     <div className='app'>
-      <h1>
-        <span role='img' aria-label='emojis fire rocket'>
-          🚀🔥
-        </span>
-        Hello Clever Programmers, let's build the Amazon store!
-        <span role='img' aria-label='emojis fire rocket'>
-          🔥🚀
-        </span>
-      </h1>
-      <Header/>
+      <Header />
       {/* Home */}
     </div>
   );
